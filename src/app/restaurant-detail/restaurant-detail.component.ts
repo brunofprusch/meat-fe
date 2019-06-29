7b4b22df0ef369db5d0e8e5671bb1e803ce1ea87
Bruno Feldmann Prusch <brunoprusch@gmail.com>
2019-06-29 16:46:04 -0300

    Finalizando aula 48

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Restaurant } from 'app/restaurants/restaurant/restaurant.model';
 import { RestaurantsService } from 'app/restaurants/restaurants.service';
 import { ActivatedRoute } from '@angular/router';
-import { empty } from 'rxjs/Observer';
 
 @Component({
   selector: 'mt-restaurant-detail',
